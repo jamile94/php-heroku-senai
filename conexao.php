2028ba7df9923ce92e7ae905bb89dfb4a45c37ed
jamile94 <72661372+jamile94@users.noreply.github.com>
2022-04-02 09:09:45 -0300

    Add files via upload
    Subindo arquivos do projeto

---
+++ conexao.php
@@ -0,0 +1,5 @@
+<?php
+
+//host, usuário, senha, nome do banco
+
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'bba4f5303a9931', 'e134358d', 'heroku_1040f7e6b4a7c19') or die('Erro ao conectar ao banco de dados');
